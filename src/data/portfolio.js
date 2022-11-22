0bdb16bfc4d0e1f5274aca8adb4705dfe956bfc9
Siddharth263 <priyanshulal2213@gmail.com>
2022-11-22 19:01:27 +0530

    working on the images

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default[
   {
     title:'Mini Project on Banking Transaction System',
-    imgUrl: "/assets/page4.jpg", 
+    imgUrl: '/assets/images/Page4.jpg', 
     stack: ['JAVA', 'MySQL', 'STS4', 'JDBC'],
     link: 'https://drive.google.com/file/d/1CzVZSFPSJEHfvmZvlmgx05RmIob9emHO/view',
   },
   {
     title:'YouTube API',
-    imgUrl: '/assets/yt_clone.jpg', 
+    imgUrl: '/assets/images/Yt_clone.jpg', 
     stack: ['JavaScript', 'HTML', 'CSS'],
     link: 'https://verdant-torrone-b6665e.netlify.app/',
   },
   {
     title:'My Portfolio',
-    imgUrl: '', 
+    imgUrl: '/assets/images/RelianceDigital.jpg', 
     stack: ['Vite','Tailwind','React'],
     link: 'https://siddharth263.github.io/',
   }
